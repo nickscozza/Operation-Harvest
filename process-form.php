72448e0ed311de7db50dc7fe821a6a685e1eb229
nickscozza <85478138+nickscozza@users.noreply.github.com>
2023-06-13 07:17:01 +1000

    Update process-form.php

--- process-form.php
+++ process-form.php
@@ -2,6 +2,36 @@
 $name = $_POST["name"];
 $email = $_POST["email"];
 $notes = $_POST["notes"];
-var_dump($name, $email, $notes);
+$host = "localhost";
+$dbname = "contact_db";
+$username = "root";
+$password = "";
+
+$conn = mysqli_connect(hostname: $host,
+username: $username, 
+password: $password, 
+database: $dbname);
+
+if (mysqli_connect_errno()) {
+    die("Connection error: " . mysqli_connect_error());
+} 
+
+echo "Connection successful.";
+
+$sql = "INSERT INTO contact (name, email, notes) 
+VALUES (?, ?, ?)";
+/*Setting the value placeholders as question marks (To prvent SQL injection attacks) */
+
+$stmt = mysqli_stmt_init($conn);
+
+if ( ! mysqli_stmt_prepare($stmt, $sql)) {
+    die(mysqli_error($conn));
+}
+
+mysqli_stmt_bind_param($stmt, "sss", $name, $email, $notes);
+mysqli_stmt_execute($stmt);
+// the type of of the inputs are all string. Therefore, we placed "sss" in the type field above (String String String)
+
+echo "Record saved."
 
 ?>
